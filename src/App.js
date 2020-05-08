171e4782afa1d3cb8140c61b0d69e7a90314975f
nathankirk91 <64616413+nathankirk91@users.noreply.github.com>
2020-05-09 09:19:48 +1000

    From Classes to Hooks
    Changed all the components to functions that use Hooks

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import FilterableProductTable from "./Components/FilterableProductTable";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <FilterableProductTable />
     </div>
   );
